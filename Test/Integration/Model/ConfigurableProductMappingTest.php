b13cfd69e70dde50f2467d7d6cb8778aa195370f
alexwanyoike <wanyoike.alexander@gmail.com>
2016-12-08 15:53:54 +0000

    Relocate fixture data to test

--- Test/Integration/Model/ConfigurableProductMappingTest.php
+++ Test/Integration/Model/ConfigurableProductMappingTest.php
@@ -2,9 +2,16 @@
 
 namespace SnowIO\ExtendedProductRepository\Test\Integration\Model;
 
+use Exception;
+use Magento\Catalog\Api\Data\ProductAttributeInterface;
+use Magento\Catalog\Api\Data\ProductExtensionFactory;
 use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Catalog\Api\ProductAttributeManagementInterface;
 use Magento\Catalog\Api\ProductAttributeRepositoryInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Eav\Api\Data\AttributeFrontendLabelInterface;
+use Magento\Eav\Api\Data\AttributeOptionInterface;
+use Magento\Eav\Api\Data\AttributeOptionLabelInterface;
 use Magento\Framework\Api\ExtensionAttributesFactory;
 use Magento\Framework\ObjectManagerInterface;
 use Magento\TestFramework\Helper\Bootstrap;
@@ -31,10 +38,72 @@ class ConfigurableProductMappingTest extends \PHPUnit_Framework_TestCase
         $this->attributeRepository = $this->objectManager->get(ProductAttributeRepositoryInterface::class);
         $this->extensionAttributesFactory = $this->objectManager->get(ExtensionAttributesFactory::class);
     }
+    
+    public function setUp()
+    {
+        //create attributes color and size and assign them the same attribute set id
+        $optionAttributes = [
+            'test_colour' => [ // options
+                $this->objectManager->create(AttributeOptionInterface::class)->setStoreLabels([
+                    $this->objectManager->create(AttributeOptionLabelInterface::class)
+                        ->setStoreId(0)
+                        ->setLabel('Red'),
+                    $this->objectManager->create(AttributeOptionLabelInterface::class)
+                        ->setStoreId(1)
+                        ->setLabel('Rot'),
+                ]),
+                $this->objectManager->create(AttributeOptionInterface::class)->setStoreLabels([
+                    $this->objectManager->create(AttributeOptionLabelInterface::class)
+                        ->setStoreId(0)
+                        ->setLabel('Blue'),
+                    $this->objectManager->create(AttributeOptionLabelInterface::class)
+                        ->setStoreId(1)
+                        ->setLabel('Blau'),
+                ]),
+            ],
+            'test_size' => [
+                $this->objectManager->create(AttributeOptionInterface::class)->setStoreLabels([
+                    $this->objectManager->create(AttributeOptionLabelInterface::class)
+                        ->setStoreId(0)
+                        ->setLabel('Small'),
+                    $this->objectManager->create(AttributeOptionLabelInterface::class)
+                        ->setStoreId(1)
+                        ->setLabel('Klein'),
+                ]),
+                $this->objectManager->create(AttributeOptionInterface::class)->setStoreLabels([
+                    $this->objectManager->create(AttributeOptionLabelInterface::class)
+                        ->setStoreId(0)
+                        ->setLabel('large'),
+                    $this->objectManager->create(AttributeOptionLabelInterface::class)
+                        ->setStoreId(1)
+                        ->setLabel('groß'),
+                ]),
+            ],
+        ];
+
+
+        $this->persistAttributeOptions('int', 'select', $optionAttributes);
+
+        //create a test product that the configurable products will link to
+        /** @var ProductRepositoryInterface $productRepository */
+        $productRepository = $this->objectManager->get(ProductRepositoryInterface::class);
+        /** @var ProductInterface$productFactory */
+        $product = $this->objectManager->get(ProductInterface::class);
+        /** @var \Magento\Catalog\Model\Product $product */
+        $product->setTypeId('simple');
+        $product->setExtensionAttributes(
+            $this->objectManager
+                ->get(ProductExtensionFactory::class)
+                ->create());
+        $product->setSku('test-product');
+        $product->setName('Test Product');
+        $product->setPrice(1.00);
+        $product->setAttributeSetId(4);
+        $productRepository->save($product);
+    }
 
     /**
      * @dataProvider getStandardCaseTestData
-     * @magentoDataFixture SnowIO/ExtendedProductRepository/Test/Integration/_files/configurable_product.php
      */
     public function testStandardCase(ProductInterface $configurableProduct)
     {
@@ -71,26 +140,6 @@ class ConfigurableProductMappingTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(0, count(array_diff($inputProductIds, $outputProductIds)));
     }
 
-    private function getProductIdFromSku(string $sku)
-    {
-        $productId = $this->productRepository->get($sku)->getId();
-
-        if ($productId === null) {
-            throw new \RuntimeException('Product id does not exist');
-        }
-
-        return $productId;
-    }
-
-    private function getAttributeIdFromCode(string $attributeCode)
-    {
-        $attributeCode = $this->attributeRepository->get($attributeCode)->getAttributeId();
-        if ($attributeCode === null) {
-            throw new \RuntimeException('Attribute code does not exist');
-        }
-        return $attributeCode;
-    }
-
     public function getStandardCaseTestData()
     {
         return [
@@ -125,5 +174,76 @@ class ConfigurableProductMappingTest extends \PHPUnit_Framework_TestCase
 
             ]
         ];
+    }
+
+    public function tearDown()
+    {
+        $attributesCodes = ['test_colour', 'test_size'];
+        /** @var ProductRepositoryInterface $productRepository */
+        $productRepository = $this->objectManager->get(ProductRepositoryInterface::class);
+        try {
+            if (null !== $productRepository->get('test-product')->getId()) {
+                $productRepository->deleteById('test-product');
+            }
+            /** @var ProductAttributeRepositoryInterface $attributeRepository */
+            $attributeRepository = $this->objectManager->get(ProductAttributeRepositoryInterface::class);
+            foreach ($attributesCodes as $attributesCode) {
+                $attributeRepository->deleteById($attributesCode);
+            }
+        } catch (Exception $e) {
+        }
+    }
+
+
+    private function persistAttributeOptions(
+        string $backendType,
+        string $frontendInput,
+        array $optionAttributes
+    ) {
+        /** @var \Magento\Catalog\Api\ProductAttributeRepositoryInterface $attributeRepository */
+        $attributeRepository = $this->objectManager->get(ProductAttributeRepositoryInterface::class);
+        /** @var ProductAttributeManagementInterface $attributeManager */
+        $attributeManager = $this->objectManager->get(ProductAttributeManagementInterface::class);
+        foreach ($optionAttributes as $attributeCode => $attributeOptions) {
+            /** @var \Magento\Catalog\Api\Data\ProductAttributeInterface $productAttribute */
+            $productAttribute = $this->objectManager->create(ProductAttributeInterface::class);
+            $productAttribute->setAttributeCode($attributeCode);
+            $productAttribute->setBackendType($backendType);
+            $productAttribute->setFrontendInput($frontendInput);
+            /** @var AttributeFrontendLabelInterface $frontEndLabelsDefaultStore */
+            $frontEndLabelsDefaultStore = $this->objectManager->create(AttributeFrontendLabelInterface::class);
+            $frontEndLabelsDefaultStore->setLabel("$attributeCode Label");
+            $frontEndLabelsDefaultStore->setStoreId(0);
+            /** @var AttributeFrontendLabelInterface $frontEndLabelsDefaultStore */
+            $frontEndLabelsTestStore = $this->objectManager->create(AttributeFrontendLabelInterface::class);
+            $frontEndLabelsTestStore->setLabel("$attributeCode Etikette");
+            $frontEndLabelsTestStore->setStoreId(1);
+            $productAttribute->setFrontendLabels([$frontEndLabelsDefaultStore, $frontEndLabelsTestStore]);
+            $productAttribute->setSourceModel('eav/entity_attribute_source_table');
+            $productAttribute->setIsUserDefined(true);
+            $productAttribute->setOptions($attributeOptions);
+            $attributeRepository->save($productAttribute);
+            $attributeManager->assign(4, 7, $attributeCode, 1);
+        }
+    }
+
+    private function getProductIdFromSku(string $sku)
+    {
+        $productId = $this->productRepository->get($sku)->getId();
+
+        if ($productId === null) {
+            throw new \RuntimeException('Product id does not exist');
+        }
+
+        return $productId;
+    }
+
+    private function getAttributeIdFromCode(string $attributeCode)
+    {
+        $attributeCode = $this->attributeRepository->get($attributeCode)->getAttributeId();
+        if ($attributeCode === null) {
+            throw new \RuntimeException('Attribute code does not exist');
+        }
+        return $attributeCode;
     }
 }
